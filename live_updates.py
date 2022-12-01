decdec596c1435082023c97179d36196f12f63d3
lumo93 <31253518+lumo93@users.noreply.github.com>
2022-12-01 09:06:19 -0500

    Code Cleanup
    Moved functions around. Now most general use edits only require changes in main and filters.

--- live_updates.py
+++ live_updates.py
@@ -1,6 +1,5 @@
 import time
 from datetime import date
-import filters
 import userdata.serviceAreaIds as serviceAreaIds
 
 
@@ -10,51 +9,73 @@ def live_mode(block):
     block_price = block['rateInfo']['priceAmount']
     block_headstart = block['startTime'] - int(time.time())
     block_rate = block_price / block_length
-    block_start = date.fromtimestamp(block['startTime']).strftime('%A'), time.strftime('%m/%d/%Y %I:%M %p', time.localtime(block['startTime']))
-    block_station = serviceAreaIds.stationlist[block['serviceAreaId']]
     try:
-        if filters.advanced_filter(block):
-            print('----------------------------------')
-            print('Scanned at', time.strftime('%m/%d/%Y %I:%M:%S %p'))
-            print('----------------------------------')
-            if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *')
-            print(block_start)
-            print('Starts in:')
-            if round((block_headstart) / 60, 2) < 60: print(round((block_headstart) / 60, 2), 'minutes')
-            if 1 < round((block_headstart) / 3600, 2) < 24: print(round((block_headstart) / 3600, 2), 'hours')
-            if 1 < round(((block_headstart) / 3600) / 24, 2): print(round(((block_headstart) / 3600) / 24, 2), 'days')
-            print(block_station)
-            print(round(block_length, 1), 'Hours')
-            print(round(block_price, 2), 'Dollars')
-            print(round(block_rate, 2), '/hr')
-            if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *')
-            print('----------------------------------')
+        print('----------------------------------')
+        print('Scanned at', time.strftime('%m/%d/%Y %I:%M:%S %p'))
+        print('----------------------------------')
+        if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *')
+        print(date.fromtimestamp(block['startTime']).strftime('%A'), time.strftime('%m/%d/%Y %I:%M %p', time.localtime(block['startTime'])))
+        print('Starts in:')
+        if round((block_headstart) / 60, 2) < 60: print(round((block_headstart) / 60, 2), 'minutes')
+        if 1 < round((block_headstart) / 3600, 2) < 24: print(round((block_headstart) / 3600, 2), 'hours')
+        if 1 < round(((block_headstart) / 3600) / 24, 2): print(round(((block_headstart) / 3600) / 24, 2), 'days')
+        print(serviceAreaIds.stationlist[block['serviceAreaId']])
+        print(round(block_length, 1), 'Hours')
+        print(round(block_price, 2), 'Dollars')
+        print(round(block_rate, 2), '/hr')
+        if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *')
+        print('----------------------------------')
     except KeyError:
         print('live mode glitch')
+
+def live_rapid(block):
+    block_length = (block['endTime'] - block['startTime']) / 3600
+    block_price = block['rateInfo']['priceAmount']
+    block_rate = block_price / block_length
+    try:
+        print('----------------------------------')
+        print('Trying again', time.strftime('%m/%d/%Y %I:%M:%S %p'))
+        print('----------------------------------')
+        print(serviceAreaIds.stationlist[block['serviceAreaId']])
+        print(round(block_length, 1), 'Hours')
+        print(round(block_rate, 2), '/hr')
+        print('----------------------------------')
+    except KeyError:
+        print('live rapid glitch')
 
 def print_history(block):
     block_length = (block['endTime'] - block['startTime']) / 3600
     block_price = block['rateInfo']['priceAmount']
     block_rate = block_price / block_length
-    block_start = date.fromtimestamp(block['startTime']).strftime('%A'), time.strftime('%m/%d/%Y %I:%M %p', time.localtime(block['startTime']))
-    block_station = serviceAreaIds.stationlist[block['serviceAreaId']]
     try:
-        if filters.advanced_filter(block):
-            with open('scandata/scanned_blocks', 'a') as f:
-                print('----------------------------------', file=f)
-                print('Scanned at', time.strftime('%m/%d/%Y %I:%M:%S %p'), file=f)
-                print('----------------------------------', file=f)
-                if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *', file=f)
-                print(block_start, file=f)
-                print(block_station, file=f)
-                print(round(block_length, 1), 'Hours', file=f)
-                print(round(block_price, 1), 'Dollars', file=f)
-                print(round(block_rate, 2), '/hr', file=f)
-                if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *', file=f)
-                print('----------------------------------', file=f)
+        with open('scandata/scanned_blocks', 'a') as f:
+            print('----------------------------------', file=f)
+            print('Scanned at', time.strftime('%m/%d/%Y %I:%M:%S %p'), file=f)
+            print('----------------------------------', file=f)
+            if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *', file=f)
+            print(date.fromtimestamp(block['startTime']).strftime('%A'), time.strftime('%m/%d/%Y %I:%M %p', time.localtime(block['startTime'])), file=f)
+            print(serviceAreaIds.stationlist[block['serviceAreaId']], file=f)
+            print(round(block_length, 1), 'Hours', file=f)
+            print(round(block_price, 1), 'Dollars', file=f)
+            print(round(block_rate, 2), '/hr', file=f)
+            if block['offerType'] == 'EXCLUSIVE': print('* * R E S E R V E D * *', file=f)
+            print('----------------------------------', file=f)
     except KeyError:
         print('print history glitch')
 
 
-
-
+def rapid_history(block):
+    block_length = (block['endTime'] - block['startTime']) / 3600
+    block_price = block['rateInfo']['priceAmount']
+    block_rate = block_price / block_length
+    try:
+        with open('scandata/scanned_blocks', 'a') as f:
+            print('----------------------------------', file=f)
+            print('Again at', time.strftime('%m/%d/%Y %I:%M:%S %p'), file=f)
+            print('----------------------------------', file=f)
+            print(serviceAreaIds.stationlist[block['serviceAreaId']], file=f)
+            print(round(block_length, 1), 'Hours', file=f)
+            print(round(block_rate, 2), '/hr', file=f)
+            print('----------------------------------', file=f)
+    except KeyError:
+        print('print history glitch')
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import requests
-import json
-import time
-import logging
-import random
+import requests, json, time, logging ,random ,yagmail
 from datetime import datetime, date
-import yagmail
 import userdata.serviceAreaIds as serviceAreaIds
 import userdata.header_data as header_data
 import userdata.json_data as json_data
-import filters
-import debug
-import live_updates
-import getServiceAreas
-import authCycle
+import filters, debug, live_updates, authCycle
 
-rapidrefresh = 3
+timehigh = 4.8
+timelow = 3.8
+
+rapidvalue = 3
+
+rapidtimehigh = 0.4
+rapidtimelow = 0.2
+
+rapidrefresh = rapidvalue
 
 logging.basicConfig(format="%(asctime)s \n\t%(message)s", datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
-yag = yagmail.SMTP(user="extra-gmail", password="app-password")
+yag = yagmail.SMTP(user="extra-email", password="app-password")
 subject = "Work Available"
+
 session = requests.Session()
 
 
@@ -29,26 +29,14 @@ def email_alert(block):
     block_rate = block_price / block_length
     block_start = f"{date.fromtimestamp(block['startTime']).strftime('%A')} {time.strftime('%m/%d/%Y %I:%M %p', time.localtime(block['startTime']))}"
     station_name = serviceAreaIds.stationlist[block['serviceAreaId']]
-    body = f"**CAUGHT A BLOCK**\n\nLocation: {station_name}\nPay: ${block_price}\nStart Time: {block_start}\nBlock Length: {block_length} hours\nRate: {block_rate}"
+    body = f"**CAUGHT A BLOCK**\n\nLocation: {station_name}\nPay: ${block_price}\nStart Time: {block_start}\nBlock Length: {block_length} hours\nRate: {round(block_rate, 2)}"
     yag.send(to='main-email', subject=subject, contents=body)
 
-
-def __getAmzDate() -> str:
-    """
-        Returns Amazon formatted timestamp as string
-        """
-    return datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
-
-def up_DATE():
-    header_data.headers["X-Amz-Date"] = __getAmzDate()
-
 print('Scanning started at', time.strftime('%I:%M:%S %p'))
-
 
 def get_offer_list():
     # Requesting list of available blocks and returning either a filtered list of blocks or an error message
     authCycle.requestId_refresh()
-    #client_time()
     global session
     response = session.post(
         "https://flex-capacity-na.amazon.com/GetOffersForProviderPost",
@@ -59,11 +47,10 @@ def get_offer_list():
     j = json.loads(response.text)
     try:
         for block in j['offerList']:
-            # Comment or uncomment bellow if you want to disable certain features
-            live_updates.live_mode(block)
-            live_updates.print_history(block)
-            debug.scan_print(block)
-            #debug.baserate_print(block)
+            if(rapidrefresh>=rapidvalue):
+                live_mode(block)
+            if(rapidrefresh<rapidvalue):
+                live_rapid(block)
         return [accept_block(block) for block in j["offerList"] if filters.advanced_filter(block)]
     except KeyError:
         try:
@@ -71,6 +58,23 @@ def get_offer_list():
         except KeyError:
             print('Disconnected.........', end='\r')
             pass
+
+def live_mode(block):
+    if filters.advanced_filter(block):
+        # Comment or uncomment bellow if you want to disable certain features
+        live_updates.live_mode(block)
+        live_updates.print_history(block)
+    if filters.print_filter(block):
+        debug.scan_print(block)
+    #if filters.baserate_filter(block):
+        #debug.baserate_print(block)
+
+def live_rapid(block):
+    if filters.advanced_filter(block):
+        # Comment or uncomment bellow if you want to disable certain features
+        live_updates.live_rapid(block)
+        #live_updates.rapid_history(block)
+
 
 def accept_block(block):
     # Accepting a block, returns status code. 200 is a successful attempt and 400 (I think, could be 404 or something else) is a failed attempt
@@ -110,22 +114,9 @@ if __name__ == "__main__":
                     keepItUp = False
                     break
             except TypeError:
-                 authCycle.authCycle()
-            '''
-            Refresh Speed:
-            Use any range for whatever speeds you are looking for
-            One method recomended no lower than one second for less risk of detection,
-            but would allow as low as 0.2
-            With the new refresh changes, it might be best to just use a low speed to scan,
-            and then when you see there are offers, use a faster speed to try and catch.
-            you will end up being throttled when you reach rate limit until you
-            wait, about an hour or more
-            '''
-            if(rapidrefresh<3):
+                authCycle.authCycle()
+            if(rapidrefresh<rapidvalue):
                 rapidrefresh+=1
-                time.sleep(random.uniform(0.2, 0.6))
+                time.sleep(random.uniform(rapidtimelow, rapidtimehigh))
             else:
-                time.sleep(random.uniform(3.8, 4.8))
-                #time.sleep(random.uniform(2.8, 3.6))
-                #time.sleep(random.uniform(0.8, 2.4))
-                #time.sleep(random.uniform(0.2, 0.6))
+                time.sleep(random.uniform(timelow, timehigh))
